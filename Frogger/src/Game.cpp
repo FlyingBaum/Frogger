a4b52c7abaa691cd0af1036250ecfa1407be7aab
FlyingBaum <kristgsus@gmail.com>
2025-04-12 00:54:01 +0200

    refactor(src): use game object class for player character + add car object

--- Frogger/src/Game.cpp
+++ Frogger/src/Game.cpp
@@ -1,8 +1,9 @@
 #include "Game.hpp"
 #include "TextureManager.hpp"
+#include "GameObject.hpp"
 
-SDL_Texture* playerTexture;
-SDL_FRect srcR, destR;
+GameObject* player;
+GameObject* car;
 
 Game::Game() {}
 
@@ -24,7 +25,6 @@ void Game::init(const char* title, int width, int height, bool isFullscreen) {
 		renderer = SDL_CreateRenderer(window, NULL);
 		if (renderer) {
 			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-			// TODO: Once SDL3 version 3.40 is out use "SDL_SetDefaultTextureScaleMode" here.
 			std::cout << "Renderer created!" << std::endl;
 		}
 
@@ -34,7 +34,8 @@ void Game::init(const char* title, int width, int height, bool isFullscreen) {
 		isRunning = false;
 	}
 
-	playerTexture = TextureManager::LoadTexture("assets/dad.png", renderer);
+	player = new GameObject("assets/dad.png", renderer, 0, 0);
+	car = new GameObject("assets/car.png", renderer, 50, 50);
 }
 
 void Game::handleEvents() {
@@ -52,18 +53,14 @@ void Game::handleEvents() {
 }
 
 void Game::update() {
-	nCount++;
-
-	destR.w = 64;
-	destR.h = 64;
-	destR.x = nCount;
-
-	std::cout << nCount << std::endl;
+	player->Update();
+	car->Update();
 }
 
 void Game::render() {
 	SDL_RenderClear(renderer);								// Clear everything with the render color.
-	SDL_RenderTexture(renderer, playerTexture, NULL, &destR); // Render player.
+	player->Render();
+	car->Render();
 	SDL_RenderPresent(renderer);							// Present all the newly rendered stuff.
 }
 

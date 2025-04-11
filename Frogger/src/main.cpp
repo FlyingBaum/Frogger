5b3cf649e384d1901c3ca7089ab7173dadcbdf11
FlyingBaum <kristgsus@gmail.com>
2025-04-11 15:18:32 +0200

    feat(src): add logic for limiting frame rate

--- Frogger/src/main.cpp
+++ Frogger/src/main.cpp
@@ -3,14 +3,30 @@
 Game* game = nullptr;
 
 int main(int argc, char* argv[]) {
+	// FPS and delay per frame.
+	const int FPS = 60;
+	const int FRAME_DELAY = 1000 / FPS;
+
+	Uint32 frameStart;
+	int frameTime;
+
 	game = new Game();
 
 	game->init("FroggerEngine", 800, 600, false);
 
 	while (game->getIsRunning()) {
+		frameStart = SDL_GetTicks();
+
 		game->handleEvents();
 		game->update();
 		game->render();
+
+		frameTime = SDL_GetTicks() - frameStart;
+
+		// Delay the frame further for consistent frame durations.
+		if (FRAME_DELAY > frameTime) {
+			SDL_Delay(FRAME_DELAY - frameTime);
+		}
 	}
 
 	game->clean();

1208f712b392943cca5f1b9d59713fa8ea6aad74
FlyingBaum <kristgsus@gmail.com>
2025-04-11 14:52:59 +0200

    Add project files.

---
+++ Frogger/src/Game.cpp
@@ -0,0 +1,83 @@
+#include "Game.hpp"
+
+SDL_Texture* playerTexture;
+SDL_FRect srcR, destR;
+
+Game::Game() {
+
+}
+
+Game::~Game() {
+
+}
+
+void Game::init(const char* title, int width, int height, bool isFullscreen) {
+	// Set flags for creating the window.
+	int flags = isFullscreen ? SDL_WINDOW_FULLSCREEN : 0;
+
+	// Create the window if the initialization of SDL worked out. Then render.
+	if (SDL_Init(0) == true) {
+		std::cout << "Subsystem initialized! ..." << std::endl;
+
+		window = SDL_CreateWindow(title, width, height, flags);
+		if (window) {
+			std::cout << "Window created!" << std::endl;
+		}
+
+		renderer = SDL_CreateRenderer(window, NULL);
+		if (renderer) {
+			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+			// TODO: Once SDL3 version 3.40 is out use "SDL_SetDefaultTextureScaleMode" here.
+			std::cout << "Renderer created!" << std::endl;
+		}
+
+		isRunning = true;
+	}
+	else {
+		isRunning = false;
+	}
+
+
+	// Create a temporary surface to load into the player texture and destroy it.
+	SDL_Surface* tmpSurface = IMG_Load("assets/dad.png");
+	playerTexture = SDL_CreateTextureFromSurface(renderer, tmpSurface);
+	SDL_SetTextureScaleMode(playerTexture, SDL_SCALEMODE_NEAREST); // Set scale mode to nearest for sharp pixel art.
+	SDL_DestroySurface(tmpSurface);
+}
+
+void Game::handleEvents() {
+	SDL_Event event;
+	SDL_PollEvent(&event);
+
+	switch (event.type) {
+	case SDL_EVENT_QUIT: 
+		isRunning = false;
+		break;
+
+	default:
+		break;
+	}
+}
+
+void Game::update() {
+	nCount++;
+
+	destR.w = 64;
+	destR.h = 64;
+	//destR.x = nCount;
+
+	std::cout << nCount << std::endl;
+}
+
+void Game::render() {
+	SDL_RenderClear(renderer);								// Clear everything with the render color.
+	SDL_RenderTexture(renderer, playerTexture, NULL, &destR); // Render player.
+	SDL_RenderPresent(renderer);							// Present all the newly rendered stuff.
+}
+
+void Game::clean() {
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	SDL_Quit();
+	std::cout << "Game Cleaned" << std::endl;
+}
---
+++ Frogger/src/Game.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "SDL3/SDL.h"
+#include "SDL3_image/SDL_image.h"
+#include <stdio.h>
+#include <iostream>
+
+class Game {
+public:
+	Game();
+	~Game();
+
+	void init(const char* title, int width, int height, bool isFullscreen = false);
+
+	void handleEvents();
+	void update();
+	void render();
+	void clean();
+
+	bool getIsRunning() { return isRunning; }
+
+private:
+	int nCount = 0;
+
+	bool isRunning;
+
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+};

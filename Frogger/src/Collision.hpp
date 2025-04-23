f7aaa64b861b9252c0de1b3bb0ee7e0171f0f0bd
FlyingBaum <kristgsus@gmail.com>
2025-04-23 12:52:48 +0200

    feat/src): add overload for AABB

--- Frogger/src/Collision.hpp
+++ Frogger/src/Collision.hpp
@@ -1,7 +1,9 @@
 #pragma once
 #include <SDL3/SDL.h>
+#include "ECS/ColliderComponent.hpp"
 
 class Collision {
 public:
 	static bool AABB(const SDL_FRect& rectA, const SDL_FRect& rectB);
+	static bool AABB(const ColliderComponent& colA, const ColliderComponent& colB);
 };

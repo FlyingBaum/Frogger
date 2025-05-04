f613ee43bc40fb2cf0f50a0e37b373e81f89f0b0
FlyingBaum <kristgsus@gmail.com>
2025-05-04 23:55:59 +0200

    refactor(src): allow flipping textures for loading and drawing

--- Frogger/src/TextureManager.hpp
+++ Frogger/src/TextureManager.hpp
@@ -4,6 +4,6 @@
 
 class TextureManager {
 public:
-	static SDL_Texture* LoadTexture(const char* fileName, bool isToBeFlipped = false);
-	static void Draw(SDL_Texture* texture, SDL_FRect src, SDL_FRect dest);
+	static SDL_Texture* LoadTexture(const char* fileName, SDL_FlipMode flipMode = SDL_FLIP_NONE);
+	static void Draw(SDL_Texture* texture, SDL_FRect src, SDL_FRect dest, SDL_FlipMode flipMode = SDL_FLIP_NONE);
 };

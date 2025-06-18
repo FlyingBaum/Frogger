66018988c19467f1f6783d28a8f5cfca6c162b08
FlyingBaum <kristgsus@gmail.com>
2025-06-18 17:28:22 +0200

    feat(ECS/KeyboardController): add buffering mechanism for player input for smoother direction changes and feel

--- Frogger/src/ECS/KeyboardController.hpp
+++ Frogger/src/ECS/KeyboardController.hpp
@@ -5,46 +5,81 @@
 #include "Components.hpp"
 
 class KeyboardController : public Component {
+private:
+    bool hasBufferedInput = false;
+    Vector2D bufferedVelocity;
+    std::string bufferedAnimation;
+    SDL_FlipMode bufferedFlip = SDL_FLIP_NONE;
+
 public:
-	TransformComponent* transform;
-	SpriteComponent* sprite;
+    TransformComponent* transform;
+    SpriteComponent* sprite;
 
-	void init() override {
-		transform = &entity->getComponent<TransformComponent>();
-		sprite = &entity->getComponent<SpriteComponent>();
-	}
+    void init() override {
+        transform = &entity->getComponent<TransformComponent>();
+        sprite = &entity->getComponent<SpriteComponent>();
+    }
 
-	void update() override {
-		if (transform->isPlayerMoving) return; // If player has not reached target location then do not process input.
+    void update() override {
+        // Process buffered input if player has stopped moving.
+        if (hasBufferedInput && !transform->isPlayerMoving) {
+            transform->velocity = bufferedVelocity;
+            transform->setPlayerInMotion();
+            sprite->Play(bufferedAnimation, true);
+            sprite->spriteFlipMode = bufferedFlip;
 
-		if (Game::event.type == SDL_EVENT_KEY_DOWN) {
-			switch (Game::event.key.key)
-			{
-			case SDLK_W:
-				transform->velocity.y = -1;
-				transform->setPlayerInMotion();
-				sprite->Play("JumpingNorth", true);
-				break;
-			case SDLK_S:
-				transform->velocity.y = 1;
-				transform->setPlayerInMotion();
-				sprite->Play("JumpingSouth", true);
-				break;
-			case SDLK_A:
-				transform->velocity.x = -1;
-				transform->setPlayerInMotion();
-				sprite->Play("JumpingWest", true);
-				sprite->spriteFlipMode = SDL_FLIP_HORIZONTAL;
-				break;
-			case SDLK_D:
-				transform->velocity.x = 1;
-				transform->setPlayerInMotion();
-				sprite->Play("JumpingEast", true);
-				sprite->spriteFlipMode = SDL_FLIP_NONE;
-				break;
-			default:
-				break;
-			}
-		}
-	}
+            hasBufferedInput = false;
+            return;
+        }
+
+        // Don't process new input if player is still moving and not close to target.
+        if (transform->isPlayerMoving && !transform->isPlayerCloseToTarget()) {
+            return;
+        }
+
+        // Handle new input.
+        if (Game::event.type == SDL_EVENT_KEY_DOWN) {
+            Vector2D newVelocity(0, 0);
+            std::string animation;
+            SDL_FlipMode flip = SDL_FLIP_NONE;
+
+            switch (Game::event.key.key) {
+            case SDLK_W:
+                newVelocity.y = -1;
+                animation = "JumpingNorth";
+                break;
+            case SDLK_S:
+                newVelocity.y = 1;
+                animation = "JumpingSouth";
+                break;
+            case SDLK_A:
+                newVelocity.x = -1;
+                animation = "JumpingWest";
+                flip = SDL_FLIP_HORIZONTAL;
+                break;
+            case SDLK_D:
+                newVelocity.x = 1;
+                animation = "JumpingEast";
+                flip = SDL_FLIP_NONE;
+                break;
+            default:
+                return;
+            }
+
+            // If player is moving but close to target, buffer the input.
+            if (transform->isPlayerMoving && transform->isPlayerCloseToTarget()) {
+                bufferedVelocity = newVelocity;
+                bufferedAnimation = animation;
+                bufferedFlip = flip;
+                hasBufferedInput = true;
+            }
+            // Otherwise process immediately.
+            else {
+                transform->velocity = newVelocity;
+                transform->setPlayerInMotion();
+                sprite->Play(animation, true);
+                sprite->spriteFlipMode = flip;
+            }
+        }
+    }
 };

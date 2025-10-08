ede154a34fddabd8a523df7ccdc4cf53c7d2caad
FlyingBaum <kristgsus@gmail.com>
2025-10-08 04:29:27 +0200

    comment out console log in AABB

--- Frogger/src/Collision.cpp
+++ Frogger/src/Collision.cpp
@@ -16,7 +16,7 @@ bool Collision::AABB(const SDL_FRect& rectA, const SDL_FRect& rectB) {
 bool Collision::AABB(const ColliderComponent& colA, const ColliderComponent& colB) {
 	if (colA.tag == "player" && colB.tag == "player") return false;
 	if (AABB(colA.collider, colB.collider)) {
-		std::cout << colA.tag << " hit: " << colB.tag << std::endl;
+		//std::cout << colA.tag << " hit: " << colB.tag << std::endl;
 		return true;
 	}
 	return false;
